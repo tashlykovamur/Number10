c8b3888a6f916a85779595de2ca63d7da4a8db0f
Аня Ташлыкова <tashlykova-anna@mail.ru>
2017-09-12 15:53:28 +0500

    Добавлен класс полинома

---
+++ Задание10/Задание10/Polinom.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задание10
+{
+    public class Polinom
+    {
+        class ListItem
+        {
+            public int Stepen { get; set; }
+            public int Koefficient { get; set; }
+            public ListItem Next { get; set; }
+        }
+
+        private ListItem first;
+
+        private ListItem last;
+
+        public void Add(int stepen, int koef)
+        {
+            if (koef == 0)
+                return;
+
+            ListItem li = new ListItem();
+            li.Stepen = stepen;
+            li.Koefficient = koef;
+
+            if (first == null)
+            {
+                first = li;
+                last = li;
+            }
+            else
+            {
+                last.Next = li;
+                last = li;
+            }
+        }
+
+        public void Show()
+        {
+            ListItem beg = first;
+
+            Console.Write("P(x) = ");
+            while (beg != null)
+            {
+                Console.Write($"{beg.Koefficient}*x^{beg.Stepen}");
+
+                if (beg.Next != null && beg.Next.Koefficient > 0)
+                    Console.Write("+");
+
+                beg = beg.Next;
+            }
+
+            Console.WriteLine();
+        }
+
+        public int Calc(int x)
+        {
+            ListItem beg = first;
+
+            int res = 0;
+
+            while (beg != null)
+            {
+                res += (int)(beg.Koefficient * Math.Pow(x, beg.Stepen));
+
+                beg = beg.Next;
+            }
+
+            return res;
+        }
+    }
+}
+
---
+++ Задание10/Задание10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Задание10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polinom polinom = new Polinom();
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    var arr = sr.ReadLine().Split();
+                    int step = int.Parse(arr[0]);
+                    int koef = int.Parse(arr[1]);
+
+                    polinom.Add(step, koef);
+                }
+            }
+
+            polinom.Show();
+
+            int x = 0;
+
+            do
+            {
+                Console.WriteLine("Введите x (для выхода введите 0)");
+                x = int.Parse(Console.ReadLine());
+
+                if (x != 0)
+                {
+                    int res = polinom.Calc(x);
+
+                    Console.WriteLine(res);
+                }
+            }
+            while (x != 0);
+        }
+    }
+}
